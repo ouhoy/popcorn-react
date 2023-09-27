3132117942ab648dc9ee8178402dc56f0b678677
ouhoy <kimkinon@gmail.com>
2023-09-27 01:02:08 +0100

    creating the stars

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import App from './App.tsx'
+// import './index.css'
+import StarRating from "./components/StarRating.tsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        {/*<App />*/}
+        <StarRating maxRating={5}/>
+        <StarRating maxRating={10}/>
+    </React.StrictMode>,
 )
